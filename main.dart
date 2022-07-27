31cd63996cc7175f4df793b7d0c32cdb5d65d9fc
Neima-Mehammednur <104683333+Neima-Mehammednur@users.noreply.github.com>
2022-07-27 19:27:20 +0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNUmber = 2;
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNUmber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex: 2
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            //flex: 1,
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$rightDiceNUmber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
